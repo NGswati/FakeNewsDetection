62e7a99f1405d6323ac91f080ef7cd26a16ea129
NGswati <94947420+NGswati@users.noreply.github.com>
2023-12-22 22:00:44 +0530

    Precision Recall Curve commit

---
+++ Precision_recall_curve.py
@@ -0,0 +1,40 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import LabelEncoder
+import matplotlib.pyplot as plt
+
+def preprocess_data(df, text_column, label_column):
+    
+    df = df.dropna(subset=[text_column, label_column]).reset_index(drop=True)
+
+    label_encoder = LabelEncoder()
+    df[label_column] = label_encoder.fit_transform(df[label_column])
+
+    return df
+
+df = pd.read_csv('news.csv')
+
+text_column = 'text'
+label_column = 'label'
+
+preprocessed_df = preprocess_data(df, text_column, label_column)
+
+X_train, X_test, y_train, y_test = train_test_split(preprocessed_df[text_column], preprocessed_df[label_column], test_size=0.2, random_state=42)
+
+model = make_pipeline(CountVectorizer(), LogisticRegression())
+model.fit(X_train, y_train)
+
+predictions = model.predict(X_test)
+
+from sklearn.metrics import precision_recall_curve, auc
+precision, recall, _ = precision_recall_curve(y_test, model.predict_proba(X_test)[:, 1])
+plt.plot(recall, precision, label=f'AUC = {auc(recall, precision):.2f}')
+plt.xlabel('Recall')
+plt.ylabel('Precision')
+plt.title('Precision-Recall Curve')
+plt.legend()
+plt.show()
